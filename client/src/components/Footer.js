1b42394571ca585d3e51ff850c8bbe71566d57c6
Stephen Gallagher <sgallagher92@gmail.com>
2022-03-08 17:25:49 +0100

    add footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="footer bg-darkBlue py-3 mt-auto">
+    <footer className="bg-darkBlue py-3 mt-auto">
       <div className="container">
-        <span className="text-muted righteous">
+        <span className="text-muted righteous" style={{ fontSize: '15px' }}>
           &copy; LinkedInk 2021 | created By Stephen Gallagher
         </span>
       </div>
