504b2ef83c2ee314c31d2ff49eadc79177150fa4
Stephen Gallagher <sgallagher92@gmail.com>
2022-05-12 16:20:07 +0800

    change base URL to deployed app

--- client/src/api/service.js
+++ client/src/api/service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const service = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: 'http://localhost:5005/api',
+  baseURL: 'https://linked-ink.herokuapp.com/api',
   withCredentials: true, // => you might need this when having the users in the app
 });
 
